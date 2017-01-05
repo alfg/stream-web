c343ecfe8c776ea991706efdc08996c532d142ac
alf <alf.g.jr@gmail.com>
2017-01-04 22:47:39 -0800

    Fix production static build routing.

--- api/server.js
+++ api/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import routes from './routes';
 import cookieParser from 'cookie-parser';
@@ -15,7 +16,12 @@ app.use('/api', routes);
 // Serve build static assets if in production or docker.
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
+  
+  app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
 }
+
 
 app.set('port', (process.env.API_PORT || 3001));
 app.listen(app.get('port'), () => {
